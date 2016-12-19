ae07187e6bfa7be6308a663b268567ea5d2835ef
root <urfin@xaner.ru>
2016-12-20 01:32:09 +0300

    ++

--- Vagrantfile
+++ Vagrantfile
@@ -4,19 +4,18 @@ VAGRANTFILE_API_VERSION = "2"
 ServersCount = 2
  
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
-#config.vm.box = "ubuntu/trusty64"
   config.vm.box = "centos/7"
  
 #  config.vm.provider "virtualbox" do |vb|
 #    vb.cpus = 2
 #  end
-#  config.vm.synced_folder ".", "/vagrant", type: "nfs"
+  config.vm.synced_folder ".", "/vagrant", type: "nfs"
 
  (1..ServersCount).each do |i| 
     config.vm.define :"kafka#{i}" do |srv|
       srv.vm.hostname = "kafka#{i}"
       srv.vm.network "private_network", ip: "192.168.50.3#{i}"
-      srv.vm.provision "shell", path: "kafka.sh" , :args => "#{i}"
+      srv.vm.provision "shell", path: "kafka.sh" , :args => "#{i} #{ServersCount}"
       end
  end
 
--- kafka.sh
+++ kafka.sh
@@ -1,16 +1,12 @@
 #!/bin/bash -ex
 echo "server $1"
 if [ ! -f /tmp/yum_installed ]; then
-  yum update -y
-  yum install mc -y
-  yum install bash-completion
-  yum install java-1.8.0-openjdk -y
-  yum install java-1.8.0-openjdk-devel -y
-  yum install wget -y
+#  yum update -y
+  yum install mc bash-completion java-1.8.0-openjdk java-1.8.0-openjdk-devel wget -y
   touch /tmp/yum_installed
 fi
 
-if [ ! -f /tmp/kafka_configured ]; then
+if [ ! -f /tmp/kafka_installed ]; then
   echo "export JAVA_HOME=/usr/lib/jvm/jre-1.8.0-openjdk" >> /etc/profile
   echo "export JRE_HOME=/usr/lib/jvm/jre" >> /etc/profile
   source /etc/profile
@@ -24,11 +20,36 @@ if [ ! -f /tmp/kafka_configured ]; then
   systemctl daemon-reload
   systemctl enable kafka-zookeeper.service
   systemctl enable kafka.service
+  systemctl stop firewalld.service
+  systemctl disable firewalld.service
+  touch /tmp/kafka_installed
+fi
+
+if [ ! -f /tmp/cluster_configured ]; then 
+  cp /vagrant/kafka/etc/hosts /etc/hosts
+  sed -r "s/broker.id=0/broker.id=${1}/g" /vagrant/kafka/opt/kafka/config/server.properties > /opt/kafka/config/server.properties.tmp
+  echo "host.name=kafka$1" >> /opt/kafka/config/server.properties.tmp  
+  servstr="zookeeper.connect="
+  cp /vagrant/kafka/opt/kafka/config/zookeeper.properties /opt/kafka/config/zookeeper.properties
+  i=0
+  while [ $i -lt $2 ] 
+  do
+    i=$[$i+1]
+    if [ $i -eq 1 ];
+    then 
+     servstr="${servstr}kafka$i:2181"
+    else
+      servstr="${servstr},kafka$i:2181"
+    fi
+    sed -r "s/zookeeper.connect=127.0.0.1:2181/${servstr}/g" /opt/kafka/config/server.properties.tmp > /opt/kafka/config/server.properties
+    echo "server.$i=kafka$i:2888:3888" >> /opt/kafka/config/zookeeper.properties
+  done
+  mkdir -p /tmp/zookeeper
+  echo $1 > /tmp/zookeeper/myid
+  chown kafka:kafka /tmp/zookeeper -R
   systemctl start kafka-zookeeper.service
   systemctl status kafka-zookeeper.service
   systemctl start kafka.service
   systemctl status kafka.service
-  systemctl stop firewalld.service
-  systemctl disable firewalld.service
-  touch /tmp/kafka_configured
+  touch /tmp/cluster_configured
 fi
