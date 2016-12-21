03a75ddc69576c2fef5ff16b8b50caec6b980164
root <urfin@xaner.ru>
2016-12-21 15:57:06 +0300

    ++

--- kafka/scripts/queue_generator.sh
+++ kafka/scripts/queue_generator.sh
@@ -3,7 +3,7 @@
 topic=${3:-kafkatopic}
 count=${2:-1000}
 delay=${1:-1}
-i=0
+i=1
 
 echo "delay is $delay, copunt is $count, topic is $topic"
 
@@ -12,9 +12,8 @@ echo "delay is $delay, copunt is $count, topic is $topic"
 
 while [ "$i" -le $count ]
 do
-((i++))
-
 echo "mseege $i"  | /opt/kafka/bin/kafka-console-producer.sh --broker-list kafka1:9092 --topic kafkatopic > /dev/null 2>&1
 echo "I said - mseege $i"
 sleep $delay
+((i++))
 done
