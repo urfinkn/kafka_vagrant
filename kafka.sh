ee310abbe1ca744d135d4997a57088b39bc954dd
root <urfin@xaner.ru>
2016-12-22 13:37:55 +0300

    ++

--- kafka.sh
+++ kafka.sh
@@ -3,6 +3,8 @@ echo "server $1"
 if [ ! -f /tmp/yum_installed ]; then
 #  yum update -y
   yum install mc bash-completion java-1.8.0-openjdk java-1.8.0-openjdk-devel wget -y
+  mkdir -p /root/.config/mc
+  cp /vagrant/sysconfs/root/.config/mc/ini /root/.config/mc/ini
   touch /tmp/yum_installed
 fi
 
--- kafka/scripts/queue_generator.sh
+++ kafka/scripts/queue_generator.sh
@@ -7,13 +7,13 @@ i=1
 
 echo "delay is $delay, copunt is $count, topic is $topic"
 
-/opt/kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic $topic --if-not-exists
+/opt/kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 2 --partitions 1 --topic $topic --if-not-exists
 
 
 while [ "$i" -le $count ]
 do
-echo "mseege $i"  | /opt/kafka/bin/kafka-console-producer.sh --broker-list kafka1:9092 --topic kafkatopic > /dev/null 2>&1
+echo "mseege $i"  | /opt/kafka/bin/kafka-console-producer.sh --broker-list kafka1:9092,kafka2:9092 --topic kafkatopic > /dev/null 2>&1
 echo "I said - mseege $i"
-sleep $delay
+##sleep $delay
 ((i++))
 done
